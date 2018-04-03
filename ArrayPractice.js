3847981f44809899b44bd52751a7b83e92143fef
guangLess <bha.guang@gmail.com>
2018-04-03 15:36:48 -0400

    arrays fin

--- ArrayPractice.js
+++ ArrayPractice.js
@@ -1,4 +1,4 @@
-//NOTE shift index when one element got removed
+//NOTE shift index when one element got removed, if not sorted use hash
 var nums = [1,1,2,2,2,2,4,5,7,7,7] // sorted 
 for (var i = 0; i< nums.length; i++) {
 	if(nums[i] === nums[i+1]) {
@@ -27,14 +27,12 @@ for(let i = 0; i < profit.length; i ++){
 
 //Rotate an array of n elements to the right by k steps.
 //For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
-
 const k = 2;
 let A = [1,2,3,4,5,6,7]
 for (let i = 0; i < (k+1); i++) {
      A.push(A.shift())
 }
 //console.log(A)
-
 
 let k1 = 3
 let A1 = [1,2,3,4,5,6,7]
@@ -52,21 +50,19 @@ function containsDuplicate (N) {
 	}
   return false 
 }
-
 const bool = containsDuplicate([3,0,1,1,2,1,4])
 //console.log(bool)
-
+/*
 function hash(keyString) {
 	var hashed = 0;
 	for (var i = 0; i< keyString.length; i++){
 		hashed += keyString.charCodeAt(i);
 	}
 	//console.log("before hash =", hashed)
-
 	return hashed % 9
 }
-
-const hashV = hash("hello")
+*/
+//const hashV = hash("hello")
 //console.log("hashV =", hashV)
 
 function containDuplicateWithHas(N){
@@ -80,11 +76,9 @@ function containDuplicateWithHas(N){
 const test2 = containDuplicateWithHas([1,3,11,2])
 //console.log(test2)
 
-
 function containDuplicateWithMap(N){
 		let dupMap = new Map();
 		for (let i = 0; i < N.length; i ++){
-
 			const dupKey = dupMap.get(N[i])
 			if (dupKey >= 0) {
 				dupMap.set(N[i], dupKey+1)
@@ -101,7 +95,7 @@ containDuplicateWithMap([1,3,11,2,1,2,2])
 
 //N = [1,2,3,3,1] -> 2
 function findSingle(N) {
-	let collect = [];
+	let collect = []//could be a hash too {n:0} -> delete key for n if duplicate
 	for (let i = 0; i < N.length; i++){
 		const index = collect.indexOf(N[i])
 		if(index > -1) collect.splice(index, 1) // remove
@@ -111,7 +105,7 @@ function findSingle(N) {
 }
 
 const x = findSingle([1])
-console.log(x)
+//console.log(x)
 
 function intersect (A, B) {
 	// compare the length , use the smaller length. 
@@ -191,10 +185,11 @@ function moveZeros(A) {
 const zeroA = moveZeros([0,0,0,1,0,3,12])
 //console.log(zeroA)
 
+//Movie two pointers
 function moveZeroTwo(A){
 	let j = 0;
 	for (let i = 0; i < A.length; i ++) {
-		console.log('i=', i, 'j=', j)
+		//console.log('i=', i, 'j=', j)
 			if (A[i] != 0) {
 				if (j < i) {
 					let temp = A[j]
@@ -204,11 +199,29 @@ function moveZeroTwo(A){
 				j++
 			}
 	}
-	console.log("2 indices A", A)
+	//console.log("2 indices A", A)
 }
 
 moveZeroTwo([1,2,0,0,0,3])//,4,0,5,0])
 
+//Rotate N x N matrix
+//r, c is row length, col length (subarray.length)
+function rotateMatrix(N,r,c){
+	const jLength = r
+	const iLength = c 
+	let newMattrix = [] // or slice the original array.
+	for(let j = 0; j < 4 ; j++) {
+		let newRow = []
+		for(let i = 0; i < 4; i++){
+			newRow.push(N[3-i][j])// 180 degree => ij -> ji : -90 degree -> ij -> i(lengh-j-1)
+		}
+		newMattrix.push(newRow)
+	}
+		console.log(newMattrix)
+}
+const test1 = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
+const testFlat = [0,1,2,3,4,5,6,7,8]
+rotateMatrix(test1,(test1.length -1), (test1[0].length -1))
 
 
 
