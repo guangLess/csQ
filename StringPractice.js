5a7fa958d9a1ab04ed5a0050dc96181456bfd2e4
guangLess <bha.guang@gmail.com>
2018-04-05 00:49:56 -0400

    reverse Intssss

--- StringPractice.js
+++ StringPractice.js
@@ -31,4 +31,34 @@ const reverseStringThree = (word) => {
 	}
 	return warray.join('')
 }
-console.log(reverseStringThree('hi poop face'))
+//console.log(reverseStringThree('hi poop face'))
+
+const reverseInts = num => {
+	let result
+	if (num<0) {
+		if (num%10 === 0) num = removeZero(num)// removed all 0s 
+		return reverseNum((num * -1)) // striped off - and 000s so -12  12, -12000 12
+	}
+	if(num === 0) return 0
+	if(num > 0)  return reverseNum(num)
+}
+
+const reverseNum = n => {
+	//n is always a possitive int
+	//since ints are not a big array, we use JS build in method
+	const nString = n.toString().split('').reverse().join('') 
+	console.log(nString,n)
+	return parseInt(nString)
+}
+
+const removeZero = n => {
+	let newN = n
+	while(n%10 === 0){
+		newN = newN / 10	
+	}
+ return newN
+}
+
+const result = reverseInts(120)
+console.log(result)
+
