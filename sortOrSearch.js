df81e7d82b8a095b990ca5e40277e966302c5457
guangLess <bha.guang@gmail.com>
2018-04-19 00:19:13 -0400

    simple bi search in JS

--- sortOrSearch.js
+++ sortOrSearch.js
@@ -12,5 +12,26 @@ const mergeSort = (m,n) => {
 	if (m.length > 0) n.concat(m)
 	return n
 }
-const mergedA = mergeSort([2,5,6], [1,6,7])
+const mergedA = mergeSort([2,5,6], [1,6,7,8,9])
 console.log(mergedA)
+
+const biSearch = isbool => {
+	return function(n){
+		let start = 0
+		let end = n
+		let mid = start + Math.floor(end/2)
+
+		while(mid > 0){
+			console.log("mid =", mid)
+			if(isbool(mid)) break
+			else{
+				// move to the right/down
+				end = mid
+				mid = start + Math.floor(end/2)
+			}
+		}
+		return mid
+	}
+}
+const isbool = (x) => x === 5
+biSearch(isbool)(11)
