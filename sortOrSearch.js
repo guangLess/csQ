0d60aa74e8c0b3146b9c0173b980c551f5f1a062
guangLess <bha.guang@gmail.com>
2018-04-18 23:57:16 -0400

    merge sorted arraies

---
+++ sortOrSearch.js
@@ -0,0 +1,9 @@
+//merge n into m, m n sorted
+const mergeSort = (m,n) => {
+	let j = 0
+	for (let i = 0; i < n.length; i++){
+		const eleI = n[i]
+		const eleJ = m[j]
+		if (eleI > eleJ) // insert j to m; j pointer move right j ++
+	}
+}
