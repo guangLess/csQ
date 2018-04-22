c33a758ecfc2fb42ec10095d8dd3eeaa0aa93331
guangLess <bha.guang@gmail.com>
2018-04-21 22:23:39 -0400

    time space complexity

--- tree🌲.js
+++ tree🌲.js
@@ -4,9 +4,12 @@ function Node(val){
 	this.left = null
 	this.right = null
 }
-
+let counter = 0
 const longPath = root => {
 	if (root===null) return 0
+	const testMax = Math.max(longPath(root.left), longPath(root.right)) + 1
+  counter++
+	console.log(testMax, "counter =", counter)
 	return Math.max(longPath(root.left), longPath(root.right)) + 1
 }
 
@@ -16,7 +19,7 @@ root.right = new Node(20)
 root.right.left = new Node(15)
 root.right.right = new Node(7)
 root.right.right.left = new Node(9)
-//console.log(root)
+
 const num = longPath(root)
 console.log(num)
 
