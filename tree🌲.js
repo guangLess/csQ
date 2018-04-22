8bbc60841c7b803058e6519d34e55a59067c755e
guangLess <bha.guang@gmail.com>
2018-04-21 22:14:13 -0400

    path max

--- tree🌲.js
+++ tree🌲.js
@@ -1,7 +1,4 @@
 
-let countL = 0
-let countR = 0
-
 function Node(val){
 	this.val = val
 	this.left = null
@@ -9,11 +6,8 @@ function Node(val){
 }
 
 const longPath = root => {
-	console.log(root.val)
-	if(root.left === null) return countL
-	if(root.right === null) return countR
-	longPath(root.left)
-	longPath(root.right)
+	if (root===null) return 0
+	return Math.max(longPath(root.left), longPath(root.right)) + 1
 }
 
 const root = new Node(3)
@@ -21,5 +15,8 @@ root.left = new Node(9)
 root.right = new Node(20)
 root.right.left = new Node(15)
 root.right.right = new Node(7)
+root.right.right.left = new Node(9)
+//console.log(root)
+const num = longPath(root)
+console.log(num)
 
-console.log(root)
