c763e320431d88f5e887a32964d83a647994747b
guangLess <bha.guang@gmail.com>
2018-04-22 21:34:40 -0400

    trees

--- tree🌲.js
+++ tree🌲.js
@@ -13,13 +13,33 @@ const longPath = root => {
 	return Math.max(longPath(root.left), longPath(root.right)) + 1
 }
 
-const root = new Node(3)
-root.left = new Node(9)
-root.right = new Node(20)
-root.right.left = new Node(15)
-root.right.right = new Node(7)
-root.right.right.left = new Node(9)
+const isBlanced = root => {
+	//if root.left === null && root.right return false
+	//if root.right === null && root.left return false
+	//console.log(root)
+	if (root.left === null && root.right === null){
+	 	console.log("check if", root)
+		return true
+	} 
+	if((root.left && root.right) && (root.left.val < root.val) && (root.val < root.right.val)){
+	 	//console.log("check if")
+	 isBlanced(root.left)
+	 isBlanced(root.right)	
+	}
+	return false
+}
 
-const num = longPath(root)
-console.log(num)
+
+const root = new Node(9)
+root.left = new Node(3)
+root.right = new Node(20)
+//root.right.left = new Node(15)
+//root.right.right = new Node(25)
+//root.right.right.left = new Node(21)
+
+//const num = longPath(root)
+//console.log(num)
+
+const checkBlanced = isBlanced(root)
+console.log(checkBlanced)
 
