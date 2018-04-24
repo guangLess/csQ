73819596b5a2efb2fc6201124df90b2c0d1983aa
guangLess <bha.guang@gmail.com>
2018-04-23 23:40:00 -0400

    suffle

---
+++ other.js
@@ -0,0 +1,13 @@
+
+const shuffle = (a) => {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1))
+      console.log("j =", j)
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+}
+
+shuffle([0,1,2,3,4])
