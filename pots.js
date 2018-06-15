2f252003e798b8a067ccd931d61098322656a89c
guangLess <bha.guang@gmail.com>
2018-06-15 17:03:41 -0400

    clean code

--- pots.js
+++ pots.js
@@ -1,53 +1,33 @@
 
 const sortByStrings = (s,t) => {
-	//console.log("hello")
 	let order = stringToObj(t)
 	for (let char of s){
-		console.log(char)
-		if (order[char] !== undefined) {
-			//console.log("xxoo", order)
-			if(order[char] >= 1) {
-				//console.log("xxoo", order)
-				order[char]++
-			} else {
-				order[char] = 1
-				//console.log("else", order)
-			}
+		if (order[char] !== undefined){
+			if (order[char] >= 1) order[char]++	
+			else order[char] = 1
 		}
 	}
-	const result = objToStringWithOrder(order)
-	console.log("result", result)
+	return objToStringWithOrder(order)
 }
 
-function stringToObj(str) {
+function stringToObj(str){
 	let obj = {}
-	for (let char of str) {
+	for (let char of str){
 		obj[char] = 0
 	}
 	return obj
 }
 
 function objToStringWithOrder(obj){
-	console.log("--->",obj)
 	let sum = []
 	for (let ele in obj){
 			const key = ele
 			const value = obj[key]
-			if (value > 1) {
-				sum.push(key.repeat(value))	
-			} 
-			if (value === 1) {
-				sum.push(key)
-			}
+			if (value > 1) sum.push(key.repeat(value))	
+			if (value === 1) sum.push(key)
 	}
-	//console.log("sum ===", sum.join(''))
 	return sum.join('')
 }
 
-//objToStringWithOrder({"h":1, "d":2})
-
-sortByStrings("weather", "therapyw") //"good", "odg"
-
-
-//const test = stringToObj("hello")
-//console.log(test)
+const test = sortByStrings("weather", "therapyw") //"good", "odg"
+console.log(test)
